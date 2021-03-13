e0d1f5aaefe4085aacfa860ab439779af2a8011e
AngeloDavid <angelodavid95@hotmail.com>
2021-03-12 22:45:24 -0500

    inicio de app

---
+++ byb2go/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Incio', url: '/folder/Inbox', icon: 'home' },
+    { title: 'Perfil', url: '/folder/Outbox', icon: 'person' },
+    { title: 'Favoritos', url: '/folder/Favorites', icon: 'star' },
+    { title: 'Historial', url: '/folder/Outbox', icon: 'archive' },
+    { title: 'Facturacion', url: '/folder/Archived', icon: 'reader' },
+    { title: 'Metodo de Pago', url: '/folder/Trash', icon: 'wallet' },
+    { title: 'Configuracion', url: '/folder/Spam', icon: 'settings' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
+  public user ={
+    name:"Romina",
+    lastname:"Sanchez",
+    type:"EXPERT++",
+    porcentage:0.5
+  };
+  
+  constructor() {}
+}
